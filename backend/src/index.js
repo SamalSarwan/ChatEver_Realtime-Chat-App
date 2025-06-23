5ca38d3b3ecbf33440ae040c2f5a428715c686ea
MegaBoyFTW <samal.sarwan.samal2@gmail.com>
2025-06-23 18:15:10 +0530

    changed files

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
+console.log(__dirname)
 // Middleware
 app.use(cors({
     origin: "http://localhost:5173",
@@ -29,7 +30,7 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV=="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname, "../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
     })
 }
 server.listen(PORT, () => {
